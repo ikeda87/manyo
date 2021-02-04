d0b5ae283c908a05df148f5b4b39bc842e3da21a
Hiroshi Ikeda <ikeda87@gmail.com>
2021-02-04 10:51:55 +0000

    add task controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,7 +45,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:title,:content)
+    params.require(:task).permit(:title,:content,:status,:deadline,:priority)
   end
 
   def set_task

4977b08950b6c903bc6a853cbc4c6d16b7f2ad43
Hiroshi Ikeda <ikeda87@gmail.com>
2021-02-04 15:38:03 +0000

    task spec model and system add

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,25 +1,40 @@
 require 'rails_helper'
+RSpec.describe Task, type: :model do
 describe 'タスクモデル機能', type: :model do
-  describe 'バリデーションのテスト' do
-    context 'タスクのタイトルが空の場合' do
-      it 'バリデーションにひっかる' do
-        task = Task.new(title: '', content: '失敗テスト')
-        expect(task).not_to be_valid
+  describe '検索機能' do
+  before do
+    @task1 = FactoryBot.create(:task,title: 'task',status: '完了')
+    @task2 = FactoryBot.create(:second_task,title: 'sample', status: '着手中')
+    @task3 = FactoryBot.create(:third_task,title: 'example', status: '着手中')
+  end
+    # 必要に応じて、テストデータの内容を変更して構わない
+    # let!(:task) { FactoryBot.create(:task, title: 'task') }
+    # let!(:second_task) { FactoryBot.create(:second_task, title: "sample") }
+    context 'scopeメソッドでタイトルの検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        # title_seachはscopeで提示したタイトル検索用メソッドである。メソッド名は任意で構わない。
+      expect(Task.search_title('タスク１')).to include(@task1)
+      expect(Task.search_title('タスク１')).not_to include(@task2)
+      expect(Task.search_title('タスク１')).not_to include(@task3)
+      expect(Task.search_title('タスク１').count).to eq 1
       end
     end
-
-    context 'タスクの詳細が空の場合' do
-      it 'バリデーションにひっかかる' do
-        task = Task.new(title: '失敗テスト', content: '')
-        expect(task).not_to be_valid
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+    expect(Task.search_status('着手中')).not_to include(@task1)
+    expect(Task.search_status('着手中')).to include(@task2)
+    expect(Task.search_status('着手中')).to include(@task3)
+    expect(Task.search_status('着手中').count).to eq 1
       end
     end
-
-    context 'タスクのタイトルと詳細に内容が記載されている場合' do
-      it 'バリデーションが通る' do
-        task = Task.new(title: '成功テスト', content: '成功テスト')
-        expect(task).to be_valid
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+    expect(Task.search_title('タスク３').search_status('完了')).to include(@task1)
+    expect(Task.search_title('タスク３').search_status('完了')).not_to include(@task2)
+    expect(Task.search_title('タスク３').search_status('完了')).not_to include(@task3)
+    expect(Task.search_title('タスク３').search_status('完了').count).to eq 1
       end
     end
   end
+ end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,21 +1,39 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  describe '新規作成機能' do
-    context 'タスクを新規作成した場合' do
-      it '作成したタスクが表示される' do
+    before do
+      FactoryBot.create(:task)
+      @task = FactoryBot.create(:second_task)
+      FactoryBot.create(:third_task)
+    end
+
+    describe '検索機能' do
+    context 'タイトルで検索をした場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        visit tasks_path
+        fill_in 'task_name', with: 'タスク01'
+        fill_in 'task_content', with: 'タスク詳細01'
+        fill_in 'task_deadline', with: '02021-01-22'
+        select '着手中', from: 'task[status]'
+        click_on '登録'
+        expect(page).to have_content 'タスク01'
       end
     end
   end
+
   describe '一覧表示機能' do
-    context '一覧画面に遷移した場合' do
-      it '作成済みのタスク一覧が表示される' do
+    context 'ステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        # ここに実装する
+        # プルダウンを選択する「select」について調べてみること
+        expect(page).to have_content 'task01'
       end
     end
-  end
-  describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
-       end
-     end
+    
+    context 'タイトルの検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'task3'
+      end
+    end
   end
 end

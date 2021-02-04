9ae6086958eab1654058e84c1a939da2eaa01f11
Hiroshi Ikeda <ikeda87@gmail.com>
2021-02-04 16:32:04 +0000

    add button

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -13,10 +13,10 @@ describe 'タスクモデル機能', type: :model do
     context 'scopeメソッドでタイトルの検索をした場合' do
       it "検索キーワードを含むタスクが絞り込まれる" do
         # title_seachはscopeで提示したタイトル検索用メソッドである。メソッド名は任意で構わない。
-      expect(Task.search_title('タスク１')).to include(@task1)
-      expect(Task.search_title('タスク１')).not_to include(@task2)
-      expect(Task.search_title('タスク１')).not_to include(@task3)
-      expect(Task.search_title('タスク１').count).to eq 1
+      expect(Task.search_title('task1')).to include(@task1)
+      expect(Task.search_title('task1')).not_to include(@task2)
+      expect(Task.search_title('task1')).not_to include(@task3)
+      expect(Task.search_title('task1').count).to eq 1
       end
     end
     context 'scopeメソッドでステータス検索をした場合' do
@@ -29,10 +29,10 @@ describe 'タスクモデル機能', type: :model do
     end
     context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
       it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
-    expect(Task.search_title('タスク３').search_status('完了')).to include(@task1)
-    expect(Task.search_title('タスク３').search_status('完了')).not_to include(@task2)
-    expect(Task.search_title('タスク３').search_status('完了')).not_to include(@task3)
-    expect(Task.search_title('タスク３').search_status('完了').count).to eq 1
+    expect(Task.search_title('task3').search_status('完了')).to include(@task1)
+    expect(Task.search_title('task3').search_status('完了')).not_to include(@task2)
+    expect(Task.search_title('task3').search_status('完了')).not_to include(@task3)
+    expect(Task.search_title('task3').search_status('完了').count).to eq 1
       end
     end
   end

4333ba03127536c9da530d8713ec3a230da4ffc8
Hiroshi Ikeda <ikeda87@gmail.com>
2021-02-08 15:02:20 +0000

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,7 @@ class UsersController < ApplicationController
   end
   private
   def user_params
-    params.require(:user).permit(:name,:email,:password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
   def set_user
     @user = User.find(params[:id])

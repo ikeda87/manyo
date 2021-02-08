7498e17eda77b9000eeea2e143783e9aeba5cdf1
Hiroshi Ikeda <ikeda87@gmail.com>
2021-02-08 14:33:19 +0000

    add controller task and application

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task,only:[ :show, :edit, :update, :destroy ]
-
+  before_action :authenticate_user, only:[:index, :new, :show, :edit, :update, :destroy]
+  before_action :ensure_current_user_task_check, only:[:show, :edit, :update, :destroy]
   def new
     @task = Task.new
   end
-
   def index
     if params[:sort_deadline]
       @tasks = Task.all.sort_deadline.pagination(params)
@@ -21,7 +21,6 @@ class TasksController < ApplicationController
       @tasks = Task.all.order(created_at: "DESC").pagination(params)
     end
    end
-
   def create
     @task = Task.new(task_params)
     if @task.save
@@ -30,13 +29,10 @@ class TasksController < ApplicationController
       render :new
     end
   end
-
   def show
   end
-
   def edit
   end
-
   def update
     if @task.update(task_params)
       redirect_to tasks_path, notice:"タスクを更新しました"
@@ -44,17 +40,14 @@ class TasksController < ApplicationController
       render :edit
     end
   end
-
   def destroy
     @task.destroy
     redirect_to tasks_path, notice:"タスクを削除しました"
   end
-
   private
   def task_params
     params.require(:task).permit(:title,:content,:status,:deadline,:priority)
   end
-
   def set_task
     @task = Task.find(params[:id])
   end
